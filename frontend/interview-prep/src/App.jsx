96d8e41324cc788a66761702bc1d582b97c36bf3
himanshumaurya45 <himanshumaurya0007@gmail.com>
2025-08-18 23:05:02 +0530

    Added all the files of frontend to github repo

---
+++ frontend/interview-prep/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to the Interview Prep AI</h1>
+      <p>Your journey to acing interviews starts here!</p>
+    </div>
+  );
+};
+
+export default App;
