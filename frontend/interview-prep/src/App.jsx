0ceb6e6fe824f67bdc45a159fc97819fdadca407
himanshumaurya45 <himanshumaurya0007@gmail.com>
2025-08-19 21:25:26 +0530

    Adding all the new files

--- frontend/interview-prep/src/App.jsx
+++ frontend/interview-prep/src/App.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to the Interview Prep AI</h1>
-      <p>Your journey to acing interviews starts here!</p>
-    </div>
+    <div>APP</div>
   );
 };
 
