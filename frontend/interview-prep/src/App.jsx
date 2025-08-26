d980174d3a92b4b4e31245418c95e2898a2160b5
himanshumaurya45 <himanshumaurya0007@gmail.com>
2025-08-26 21:51:12 +0530

    Committed on 26 Aug 2025 after updating it

--- frontend/interview-prep/src/App.jsx
+++ frontend/interview-prep/src/App.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast';
+
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Home/Dashboard';
+import InterviewPrep from './pages/InterviewPrep/interviewPrep';
 
 const App = () => {
   return (
-    <div>APP</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signUp" element={<SignUp/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/interview-prep/:sessionId" element={<InterviewPrep/>}/>
+        </Routes>
+      </Router>
+
+      <Toaster toastOptions={{
+        className:"",
+        style:{
+          fontSize:"13px",
+        },
+      }}/>
+    </div>
   );
 };
 
